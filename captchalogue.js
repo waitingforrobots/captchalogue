2c757a1fc5e1665d186d54cb3ef0176753352cbf
June <laurawdougherty@gmail.com>
2024-03-22 16:39:33 -0700

    working buttons on main page!!

--- captchalogue.js
+++ captchalogue.js
@@ -1,5 +1,4 @@
-
-// == Add space ==
+// == Ad space ==
 
 const adData = [
   ["zarabanner.jpg", "https://ko-fi.com/zaranerd"],
@@ -15,3 +14,23 @@ adImg.src = "./ads/" + randomAd[0]
 adLink.href = randomAd[1]
 
 // == Page Body ==
+
+//buttonnav
+const readerButt = document.getElementById('readbutton');
+const archButt = document.getElementById('archbutton');
+const fanButt  = document.getElementById('fanbutton');
+const beyondButt = document.getElementById('beyondbutton');
+const newsButt = document.getElementById('newsbutton');
+
+let currPage = window.location.href;
+
+const redirect = (page) => {
+	window.location.href = currPage.replace("index.html", `${page}.html`)
+}
+
+readerButt.addEventListener('click', () => redirect('newreader'));
+archButt.addEventListener('click', () => redirect('archive'));
+fanButt.addEventListener('click', () => redirect('fanwork'));
+beyondButt.addEventListener('click', () => redirect('beyond'));
+newsButt.addEventListener('click', () => redirect('news'));
+
